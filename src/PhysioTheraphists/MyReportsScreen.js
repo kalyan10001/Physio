8b1123ea011e18c5378301ee767243dc4f1e8f34
kalyan10001 <venkatakalyan1000@gmail.com>
2025-07-03 17:07:56 +0530

    css

--- src/PhysioTheraphists/MyReportsScreen.js
+++ src/PhysioTheraphists/MyReportsScreen.js
@@ -3,7 +3,6 @@ import {
   View,
   Text,
   StyleSheet,
-  SafeAreaView,
   TouchableOpacity,
   FlatList,
   Image,
@@ -11,6 +10,7 @@ import {
   ScrollView,
   Dimensions,
 } from 'react-native';
+import { SafeAreaView } from 'react-native-safe-area-context';
 
 const { width } = Dimensions.get('window');
 
